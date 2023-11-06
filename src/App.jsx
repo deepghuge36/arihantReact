ce30046129e55818d7eba01be178351dfb3528b6
Dipak Ghuge <deepghuge@gmail.com>
2023-11-06 17:33:37 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,17 @@
-
-import reactLogo from './assets/react.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  return (
-    <>
-      <div>
-        
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Arihant Demo</h1>
-
-    </>
-  )
+	return (
+		<>
+			<div>
+				<a href="https://react.dev" target="_blank">
+					<img src={reactLogo} className="logo react" alt="React logo" />
+				</a>
+			</div>
+			<h1>Arihant Demo with New update</h1>
+		</>
+	);
 }
 
-export default App
+export default App;
